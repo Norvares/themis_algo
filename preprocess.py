c91db64d8fb3f15d1548f2221bd222687689c63b
Finn Masurat <finn.masurat@hotmail.de>
2016-05-19 15:20:35 +0200

    onlyNounsAndNames in test

--- preprocess.py
+++ preprocess.py
@@ -9,14 +9,11 @@ example = 'Obama is fighting Putin'
 
 def onlyNounsAndNames(content):
     try:
-        for item in content:
-            tokenized = nltk.word_tokenize(item)
-            tagged = nltk.pos_tag(tokenized)
-            words = ' '.join(word for word,tag in tagged if tag in ('NN', 'NNP'))
-            print words
+        tokenized = nltk.word_tokenize(content)
+        tagged = nltk.pos_tag(tokenized)
+        words = ' '.join(word for word,tag in tagged if tag in ('NN', 'NNP'))
+        return words
 
 
     except Exception, e:
         print str(e)
-
-onlyNounsAndNames(example)
