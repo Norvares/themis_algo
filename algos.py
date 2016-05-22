bda3c34707a567da94eda69666a229c5e44ce6ea
Your Name <you@example.com>
2016-05-22 19:44:59 +0200

    print of the score added

--- algos.py
+++ algos.py
@@ -36,7 +36,7 @@ def kmeans(cursor, limit, n_features, true_k, init, n_init, max_iter, tol, preco
 
     model.fit_predict(X)
     labels = model.labels_
-    score = metrics.silhouette_score(X, labels, metric='euclidean')
+    print(score = metrics.silhouette_score(X, labels, metric='euclidean'))
     evaluationContent = {}
     evaluationContent['SilhouetteCoefficient'] = score
     jsn['evaluation'] = evaluationContent
