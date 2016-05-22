84e58b250609f5bd396fbb2d3aa331eaae6481da
Finn Masurat <finn.masurat@hotmail.de>
2016-05-22 21:45:21 +0200

    removing mr

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ def onlyNounsAndNames(content):
     try:
         tokenized = nltk.word_tokenize(content)
         tagged = nltk.pos_tag(tokenized)
-        words = ' '.join(word for word,tag in tagged if tag in ('NN', 'NNP') and word != 'Mr') 
+        words = ' '.join(word for word,tag in tagged if tag in ('NN', 'NNP') and word != ' Mr') 
         return words
 
     except Exception, e:
