f4e9b3cfaae3bbac30fbc150929c7ad1369541fd
Finn Masurat <finn.masurat@hotmail.de>
2016-05-23 10:13:05 +0200

    wip

--- preprocess.py
+++ preprocess.py
@@ -28,7 +28,7 @@ def lemmatizer(content):
 def stemmer(content):
     try:
         stemmer = SnowballStemmer()
-        words = ' '.join(stemmer.stem(word) for word in tagged)
+        words = ' '.join([stemmer.stem(word) for word in content.split()])
         return words
     except Exception, e:
         print str(e)
