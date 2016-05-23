1425e557f1419e2dead5f0786d5aa7c97d616464
Jakob Joachim <jak.joachim@gmail.com>
2016-05-23 12:02:24 +0200

    done

--- algos.py
+++ algos.py
@@ -66,7 +66,6 @@ def kmeans(cursor, limit, n_features, true_k, init, n_init, max_iter, tol, preco
 
     model.fit_predict(X)
     labels = model.labels_
-    print(labels)
     score = metrics.silhouette_score(X, labels, metric='euclidean')
     evaluationContent = {}
     evaluationContent['SilhouetteCoefficient'] = score
