112fb08a7674c498836303859c8fad28435e6aef
Your Name <you@example.com>
2016-05-28 14:17:33 +0200

    data2csv finished

--- helper.py
+++ helper.py
@@ -1,9 +1,13 @@
 import rethinkdb as r
 from csvkit.convert.js import json2csv
+from collections import OrderedDict
+import StringIO, json
 
 def dataToCSV():
     c = r.connect()
-    output = r.db("db").table("trueKExperiment") \
-    .group(r.row["config"]["n_cluster"]) \
-    .ungroup().merge(r.row["evaluation"]["SilhouetteCoefficient"]).run(conn)
-    print (output)
+    cursor = r.db("themis").table("trueKExperiment").run(c)
+    data  = json.dumps([OrderedDict([
+        ["clusters", item["config"]["n_clusters"]],
+        ["coefficient", item["evaluation"]["SilhouetteCoefficient"]]]) for item in cursor])
+    converted = json2csv(StringIO.StringIO(data))
+    print(converted)
