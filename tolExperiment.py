6d6f3962d50e9db830ffdc32daaa4e63e57a107a
Jakob Joachim <jak.joachim@gmail.com>
2016-05-28 14:31:14 +0200

    ready to be tested

--- tolExperiment.py
+++ tolExperiment.py
@@ -2,9 +2,9 @@ import algos
 import rethinkdb as r
 
 c = r.connect()
-i = 2
-while (i < 1000):
+i = 0
+while (i < 0.002):
     cursor = r.db("themis").table("pages").limit(1000).run(c)
-    jsonResult = algos.kmeans(cursor, 1000, 20, i, 'k-means++', 10, 1000, 0.0001, 'auto', 0, None, True, 1, None)
-    r.db("themis").table("trueKExperiment").insert(jsonResult).run(c)
-    i = i+1
+    jsonResult = algos.kmeans(cursor, 1000, 20, 250, 'k-means++', 10, 1000, i, 'auto', 0, None, True, 1, None)
+    r.db("themis").table("tolExperiment").insert(jsonResult).run(c)
+    i = i+0.00002
